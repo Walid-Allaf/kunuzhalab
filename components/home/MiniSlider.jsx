9eb52fcfaa70a32e1884bcafe4890e0e863b34cc
walid <walid3llaf053@gmail.com>
2024-05-08 05:14:41 +0300

    remove document

--- components/home/MiniSlider.jsx
+++ components/home/MiniSlider.jsx
@@ -11,27 +11,27 @@ import { useEffect, useState } from "react";
 const data = [MNIISLIDER, MNIISLIDER, MNIISLIDER, MNIISLIDER, MNIISLIDER];
 
 export default function MiniSlider() {
-  let nums = document.querySelectorAll(".nums .num");
-  let section = document.querySelector(".three");
-  let started = false;
-  window.onscroll = function () {
-    if (window.scrollY >= section.offsetTop) {
-      if (!started) {
-        nums.forEach((num) => startCount(num));
-      }
-      started = true;
-    }
-  };
+  // let nums = document.querySelectorAll(".nums .num");
+  // let section = document.querySelector(".three");
+  // let started = false;
+  // window.onscroll = function () {
+  //   if (window.scrollY >= section.offsetTop) {
+  //     if (!started) {
+  //       nums.forEach((num) => startCount(num));
+  //     }
+  //     started = true;
+  //   }
+  // };
 
-  function startCount(el) {
-    let goal = el.dataset.goal;
-    let count = setInterval(() => {
-      el.textContent++;
-      if (el.textContent == goal) {
-        clearInterval(count);
-      }
-    }, 2000 / goal);
-  }
+  // function startCount(el) {
+  //   let goal = el.dataset.goal;
+  //   let count = setInterval(() => {
+  //     el.textContent++;
+  //     if (el.textContent == goal) {
+  //       clearInterval(count);
+  //     }
+  //   }, 2000 / goal);
+  // }
   const [width, setWidth] = useState(0);
   useEffect(() => {
     setWidth(window.innerWidth);
